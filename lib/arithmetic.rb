7271a62096a46e3b960f748f486c17f4c37f9b63
devux <devakumar.mj@gmail.com>
2015-10-15 17:47:29 +0530

    initial commit for gem

---
+++ lib/arithmetic.rb
@@ -0,0 +1,37 @@
+require "arithmetic/version"
+
+class Arithmetic
+  # Your code goes here...
+  def addition e,f
+  	a=e
+  	b=f
+  	c=a+b
+  	puts "value of a is #{a}"
+  	puts "value of a is #{b}"
+  	puts "Addition value of a and b is #{c}"
+  end
+  def subtraction a,b
+  	a=e
+  	b=f
+  	c=a-b
+  	puts "value of a is #{a}"
+  	puts "value of a is #{b}"
+  	puts "subtraction value of a and b is #{c}"
+  end
+  def mutiplication a,b
+  	a=e
+  	b=f
+  	c=a*b
+  	puts "value of a is #{a}"
+  	puts "value of a is #{b}"
+  	puts "mutiplication value of a and b is #{c}"
+  end
+  def division a,b
+  	a=e
+  	b=f
+  	c=a/b
+  	puts "value of a is #{a}"
+  	puts "value of a is #{b}"
+  	puts "mutiplication value of a and b is #{c}"
+  end
+end
---
+++ lib/arithmetic/version.rb
@@ -0,0 +1,3 @@
+class Arithmetic
+  VERSION = "0.1.0"
+end
